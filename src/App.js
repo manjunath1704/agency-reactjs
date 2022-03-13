66cab99f3f4fbc6f12c11019e3970f9cec8c2ede
manjunath1704 <imfoofmanju@gmail.com>
2022-03-13 23:57:20 +0530

    Setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import './scss/style.scss'
+function App() {
+  return (
+    <section className='bg-red'>
+      <Container>
+        <Row>
+          <Col>hello</Col>
+        </Row>
+      </Container>
+    </section>
+  );
+}
+export default App;
